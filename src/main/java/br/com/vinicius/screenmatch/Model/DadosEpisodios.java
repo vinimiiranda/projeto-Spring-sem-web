c65bc1d9d22cfee0d896065492e9fb2d197afda2
vinicius de miranda melo <viniciusmmelo03@gmail.com>
2025-07-21 20:18:13 -0300

    Feita a alteracao de imdRating para imdbRating

--- src/main/java/br/com/vinicius/screenmatch/Model/DadosEpisodios.java
+++ src/main/java/br/com/vinicius/screenmatch/Model/DadosEpisodios.java
@@ -6,6 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosEpisodios(@JsonAlias("Title") String titulo,
                              @JsonAlias("Episode") Integer numero,
-                             @JsonAlias("imdRating")String avaliacao,
+                             @JsonAlias("imdbRating")String avaliacao,
                              @JsonAlias("Released")String dataLancamento) {
 }
